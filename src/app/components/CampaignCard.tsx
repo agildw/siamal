51040c0c4960c4024b0d654dcf0d998c305aedd5
Agil Dwiki Yudistira <yudistiraagil24@gmail.com>
2024-07-03 20:38:51 +0700

    chore: Update CampaignCard component
    Refactor CampaignCard component to handle both CampaignWithDonations and CampaignWithTotalDonations types, and update UI accordingly

--- src/app/components/CampaignCard.tsx
+++ src/app/components/CampaignCard.tsx
@@ -5,20 +5,33 @@ import {
   CardMedia,
   LinearProgress,
 } from "@mui/material";
-import { handleAmount, handleTruncate } from "~/app/utils/util";
+import { calculateFunds, handleAmount, handleTruncate } from "~/app/utils/util";
 import StatusChip from "./StatusChip";
-import type { CampaignWithDonations } from "types/campaign";
+import type {
+  CampaignWithDonations,
+  CampaignWithTotalDonations,
+} from "types/campaign";
+import { HeartIcon, ClockIcon } from "@heroicons/react/24/outline";
+import moment from "moment";
 
 interface CampaignCardProps {
-  campaign: CampaignWithDonations;
+  campaign: CampaignWithDonations | CampaignWithTotalDonations;
   showStatus?: boolean;
 }
 
 const CampaignCard = ({ campaign, showStatus = false }: CampaignCardProps) => {
-  const total = campaign.donations.reduce(
-    (acc, donation) => acc + donation.amount,
-    0,
-  );
+  let total = 0;
+  let totalDonations = 0;
+  if ("donations" in campaign) {
+    total = calculateFunds(campaign);
+
+    totalDonations = campaign.donations.filter(
+      (donation) => donation.status === "PAID",
+    ).length;
+  } else {
+    total = campaign.totalFunds;
+    totalDonations = campaign.totalDonations;
+  }
 
   const progress = (total / campaign.target) * 100;
 
@@ -33,24 +46,48 @@ const CampaignCard = ({ campaign, showStatus = false }: CampaignCardProps) => {
           className="h-56 w-full object-cover"
         />
         {/* <CardContent className="flex h-60 flex-col justify-between"> */}
-        <CardContent className="flex flex-col justify-between">
+        <CardContent
+          className={`flex flex-col justify-between ${showStatus ? "h-60" : "h-52 "}`}
+        >
           <div className="flex flex-col">
             {showStatus && <StatusChip status={campaign.status} />}
             <p className="mt-2 text-lg font-bold">
-              {handleTruncate(campaign.title, 20)}
+              {handleTruncate(campaign.title, 50)}
             </p>
-            <p className=" h-16 text-gray-500">
-              {handleTruncate(campaign.description, 50)}
+            <p className=" text-gray-500">
+              {handleTruncate(campaign.description, 30)}
             </p>
           </div>
-          <div className="flex flex-col">
+          <div className="mt-2 flex flex-col space-y-2">
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-row items-center space-x-2 rounded-lg bg-gray-100 p-1.5">
+                <HeartIcon className="h-5 w-5 text-gray-500" />
+                <div className="flex flex-row items-center space-x-1">
+                  <p className="text-xs font-bold text-gray-700">
+                    {Math.abs(totalDonations)}
+                  </p>
+                  <p className="text-xs text-gray-500">Donasi</p>
+                </div>
+              </div>
+              <div className="flex flex-row items-center space-x-2">
+                <ClockIcon className="h-5 w-5 text-gray-500" />
+                <div className="flex flex-row items-center space-x-1">
+                  {/* tampilkan sisa hari */}
+                  <p className="text-xs font-bold text-gray-700">
+                    {Math.abs(moment(campaign.endDate).diff(moment(), "days"))}
+                  </p>
+                  <p className="text-xs text-gray-500">Hari lagi</p>
+                </div>
+              </div>
+            </div>
             <LinearProgress
               variant="determinate"
               value={progress > 100 ? 100 : progress}
-              className="mt-4"
+              className="h-2 rounded-full p-1"
             />
-            <p className="mt-2 text-sm text-gray-500">
-              Rp{handleAmount(total)} raised of Rp
+            <p className="text-sm text-gray-500">
+              <span className="font-bold">Rp{handleAmount(total)} </span>
+              terkumpul dari Rp
               {handleAmount(campaign.target)}
             </p>
           </div>
--- src/app/utils/util.ts
+++ src/app/utils/util.ts
@@ -1,14 +1,44 @@
-export const handleAmount = (amount: number) => {
+import type { CampaignWithDonations } from "types/campaign";
+
+export const handleAmount = (amount: number, digit = 1) => {
   // change thousands to k and millions to m
   if (amount >= 1000000) {
-    return `${(amount / 1000000).toFixed(1)}M`;
+    return `${(amount / 1000000).toFixed(digit)}M`;
   }
   if (amount >= 1000) {
-    return `${(amount / 1000).toFixed(1)}K`;
+    return `${(amount / 1000).toFixed(digit)}K`;
   }
   return `${amount}`;
 };
 
 export const handleTruncate = (text: string, length: number) => {
   return text.length > length ? `${text.substring(0, length)}...` : text;
+};
+
+export const handleStatisticalAmount = (number: number) => {
+  let roundedNumber;
+  if (number < 50) {
+    roundedNumber = Math.round(number / 10) * 10;
+  } else {
+    roundedNumber = Math.min(Math.round(number / 50) * 50, number);
+  }
+
+  if (roundedNumber >= 1000000) {
+    return (roundedNumber / 1000000).toFixed(0) + "M";
+  } else if (roundedNumber >= 1000) {
+    return (roundedNumber / 1000).toFixed(0) + "K";
+  } else {
+    return roundedNumber.toString();
+  }
+};
+
+export const calculateFunds = (campaign: CampaignWithDonations) => {
+  return campaign.donations
+    .filter((donation) => donation.status === "PAID")
+    .reduce((acc, donation) => acc + donation.amount, 0);
+};
+
+export const countPaidDonations = (campaign: CampaignWithDonations) => {
+  return campaign.donations.filter((donation) => donation.status === "PAID")
+    .length;
 };
